d82cb97063f13f316a35bbfbb8a81c27e24377ce
Jason White <jason.white@marylandauto.net>
2019-06-25 13:42:14 -0400

    adding files for LAMP stack

---
+++ scripts/trusty-lamp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update ubuntu with any patches
+apt-get update
+
+# Tools
+apt-get install -y nano git unzip screen
+
+# Apache
+apt-get install -y apache2
+
+# PHP
+apt-get install -y php php-cli php-common php-devel php-mysql
+
+# MySQL
+apt-get install -y mysql mysql-server mysql-devel
+
+# Download Starter Content
+
